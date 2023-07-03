8763c6f5684f2a169d7a0bbd5c5e0bc23ccd6c08
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-07-04 00:57:49 +0400

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+const getUserName = () => {
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hello,' + userName + '!');
+}
+
+export { getUserName };
+
+
