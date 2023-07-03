d8b3cecaa12d8e5365fb3881eff3332ebb7b0226
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-07-04 01:58:43 +0400

    lintered

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
+
 const getUserName = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello,' + userName + '!');
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
-export { getUserName };
-
-
+export default getUserName;
