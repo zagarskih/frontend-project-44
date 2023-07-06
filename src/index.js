675b82ef3bdc50a9d1637919fe90a5624458b8e2
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-07-06 16:02:18 +0400

    rewrite index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => {
+const ROUNDS_COUNT = 3;
+
+const getRandomNumber = (max, min = 0) => Math.floor(min + Math.random() * (max - min));
+
+const startGame = (task, getGameData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName;
+  console.log(task);
+
+  for (let i = 0; i < ROUNDS_COUNT; i += 1) {
+    const [question, rightAnswer] = getGameData();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === String(rightAnswer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
-const getRandomNumber = (max, min = 0) => Math.floor(min + Math.random() * (max + 1 - min));
-
-const getAnswer = () => {
-  const answer = readlineSync.question('Your answer: ');
-  return answer;
-};
-
-export default getUserName;
-export { getRandomNumber, getAnswer };
+export default startGame;
+export { getRandomNumber };
