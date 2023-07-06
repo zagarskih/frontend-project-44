a975f7ca012a81bd5b1b8a57546d254c11cc6612
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-07-06 16:01:24 +0400

    rewrite progression.js

--- games/progression.js
+++ games/progression.js
@@ -1,45 +1,32 @@
-import getUserName, { getRandomNumber, getAnswer } from '../src/index.js';
+import startGame, { getRandomNumber } from '../src/index.js';
 
-const playGame = () => {
-  const userName = getUserName();
+const task = 'What number is missing in the progression?';
 
-  console.log('What number is missing in the progression?');
-
+const getGameData = () => {
   const maxFirstNumber = 20;
   const minStep = 2;
   const maxStep = 10;
   const numsCount = 10;
-  let i = 0;
 
-  while (i < 3) {
-    const step = getRandomNumber(maxStep, minStep);
-    const firstNumber = getRandomNumber(maxFirstNumber);
-    const indexOfEmpty = getRandomNumber(numsCount);
+  const step = getRandomNumber(maxStep, minStep);
+  const firstNumber = getRandomNumber(maxFirstNumber);
+  const indexOfEmpty = getRandomNumber(numsCount);
 
-    const arrOfNums = [];
-    let elemOfProgression = firstNumber;
+  const arrOfNums = [];
+  let elemOfProgression = firstNumber;
 
-    for (let j = 0; j < numsCount; j += 1) {
-      elemOfProgression += step;
-      arrOfNums.push(elemOfProgression);
-    }
-
-    const correctAnswer = arrOfNums[indexOfEmpty];
-    arrOfNums[indexOfEmpty] = '..';
-
-    console.log(`Question: ${arrOfNums.join(' ')}`);
-    const answer = Number(getAnswer());
-
-    if (correctAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    }
-    i += 1;
-    if (i === 3) console.log(`Congratulations, ${userName}!`);
+  for (let i = 0; i < numsCount; i += 1) {
+    elemOfProgression += step;
+    arrOfNums.push(elemOfProgression);
   }
+
+  const rightAnswer = arrOfNums[indexOfEmpty];
+  arrOfNums[indexOfEmpty] = '..';
+  const question = `${arrOfNums.join(' ')}`;
+
+  return [question, rightAnswer];
 };
 
-export default playGame;
+const startProgressionGame = () => startGame(task, getGameData);
+
+export default startProgressionGame;
